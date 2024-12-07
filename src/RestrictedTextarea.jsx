1bcba6c112f2c294ad66183b22f17bd1129d0d1f
wada.t00 <wada.t00@DTC23024>
2024-12-07 16:05:57 +0900

    完成

--- src/RestrictedTextarea.jsx
+++ src/RestrictedTextarea.jsx
@@ -1,16 +1,22 @@
 import {useState} from "react";
+import "./RestrictedTextarea.css"
 
 export default function RestrictedTextarea() {
-    const[count, setCount] = useState(0);
+    const maxCount = 250;
+
+    const [count, setCount] = useState(0);
 
     const handleChange = (e) => {
         setCount(e.target.value.length);
     }
 
+
     return (
-        <div>
-            <textarea onChange={handleChange} />
-            <span>{count}</span>
+        <div className={`restricted-textarea ${count === maxCount ? 'restricted-textarea--error' : ''}`}>
+            <textarea placeholder="Start typing ..."
+                      className={`restricted-textarea__textarea ${count === maxCount ? 'restricted-textarea__textarea--error' : 'restricted-textarea__textarea--success'}`}
+                      onChange={handleChange} maxLength={maxCount}/>
+            <span className="restricted-textarea__count">{count} / {maxCount}</span>
         </div>
     )
 }
